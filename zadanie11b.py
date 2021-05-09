4122c9eb76b62dc2a647aa9400db200e0cfdfda2
kcptr <kcptr@users.noreply.github.com>
2021-05-09 12:42:55 +0000

    zadanie11b

---
+++ zadanie11b.py
@@ -0,0 +1,6 @@
+samoloty = ['cessna', 'spitfire']
+ilosc = [7, 1]
+
+for idx in range( len(samoloty) ) :
+    print('idx: {0} {1}'.format(idx, samoloty[idx]))
+    print(samoloty[idx] + ' ma ilosc okien ' + str(ilosc[idx]))
