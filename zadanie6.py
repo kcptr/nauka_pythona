9ce0d568efc3396478ab5f30bbfa76b00a3ba81e
kcptr <kcptr@users.noreply.github.com>
2021-05-09 11:49:04 +0000

    zadanie6

---
+++ zadanie6.py
@@ -0,0 +1,16 @@
+# imie = 'Ala'
+# ile = 1
+# zwierze = 'kot'
+# if ile == 1:
+#     print ("{0} ma {1} kota".format(imie, ile))
+# elif ile > 1:
+#     print ("{0} ma {1} koty".format(imie, ile))
+
+
+marka = 'BMW'
+koni = 230
+model = 'X5'
+if koni == 300:
+    print ("jesli {0} {1} ma {2} koni to jest szybkie".format(marka, model, koni))
+if koni < 300:
+    print ("jesli {0} {1} ma {2} koni to jest wolne".format(marka, model, koni))
