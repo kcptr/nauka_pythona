1a194e8a16f8af2755240b3d6e91602dfd493787
kcptr <kcptr@users.noreply.github.com>
2021-05-09 11:45:38 +0000

    zadanie5

---
+++ zadanie5.py
@@ -0,0 +1,10 @@
+marka = 'Peugeot'
+ilosc_drzwi = 5
+pojemnosc = 1.3
+
+marka_up = marka.upper()
+marka_low = marka.lower()
+print('Samochod ' + marka)
+print(marka_up)
+print(marka_low)
+print("Pojemnosc " + str(pojemnosc * 2))
