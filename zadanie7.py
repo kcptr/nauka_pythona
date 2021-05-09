cd6c073d25a240be8c11f4e6365b04a805f7a221
kcptr <kcptr@users.noreply.github.com>
2021-05-09 11:49:37 +0000

    zadanie7

---
+++ zadanie7.py
@@ -0,0 +1,18 @@
+# imie = "Ala"
+# ile_kotow = 3
+#
+#
+# i = 0
+# while i < ile_kotow:
+#     print("Przyszedl kot. {0} ma {1} kotow".format(imie, i))
+#     i = i + 1
+
+
+aplikacja = "Program"
+ile_opcji = 14
+
+
+i = 0
+while i < ile_opcji:
+    print("Dodano 2 opcje. {0} ma {1} opcji".format(aplikacja, i))
+    i = i + 2
