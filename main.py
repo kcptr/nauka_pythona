7de24ffeefe570b58aebf622f573ad70bb3f2817
kcptr <kcptr@users.noreply.github.com>
2021-05-09 11:50:26 +0000

    main

---
+++ main.py
@@ -0,0 +1,22 @@
+
+
+# imie= "Ania"
+#
+# ilosc_ksiazek = 10 # liczba calkowita
+# srednia = 4.5 # zmienna przecinkowa
+#
+# print("Hello world " + imie)
+# print('ilosc ksiazek: ' + str(ilosc_ksiazek))
+# print('Srednia: ' + str(srednia))
+
+# CamelCase <- KLASY
+# snake_case <- zmienne
+
+samochod= "Audi"
+
+ilosc_koni = 350 # liczba calkowita
+spalanie = 9.5 # zmienna przecinkowa
+
+print("Najfajniejszy samochod: " + samochod)
+print('moc: ' + str(ilosc_koni))
+print('Spalanie: ' + str(spalanie))
